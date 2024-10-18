cb0d1c9f064bbea8ecf57e31cbabb1454239b008
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2024-10-18 03:11:38 +0100

    simplify code

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,13 +1,10 @@
 -- Create a stored procedure ComputeAverageScoreForUser to compute and store the average score for a student
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
 DELIMITER //
-
-CREATE PROCEDURE ComputeAverageScoreForUser(
-  IN user_id INT
-)
+CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
-  UPDATE users
-  SET average_score = (SELECT AVG(score) FROM corrections WHERE user_id = users.id)
-  WHERE id = user_id;
-END//
-
+    UPDATE users
+    SET average_score = (SELECT AVG(score) FROM corrections WHERE corrections.user_id = user_id)
+    WHERE id = user_id;
+END;//
 DELIMITER ;
